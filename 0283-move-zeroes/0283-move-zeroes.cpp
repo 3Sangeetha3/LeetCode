f6fa4a89fe7879f1ed5ae2076e23378e345a6ad7
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-01-23 17:51:34 +0530

    Time: 0 ms (100%), Space: 23.8 MB (75.96%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -1,16 +1,24 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        //Brute force 
+        //optimal approach 
         int n = nums.size();
-        vector<int> nonZero(n, 0);
+        int j=-1;
 
-        int idx = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] != 0){
-                nonZero[idx++] = nums[i];
+            if(nums[i] == 0){
+                j = i;
+                break;
             }
         }
-        nums = nonZero;
+        if (j == -1) {
+            return;
+        }
+        for(int i=j+1;i<n;i++){
+            if(nums[i] != 0){
+                swap(nums[i], nums[j]);
+                j++;
+            }
+        }
     }
 };
