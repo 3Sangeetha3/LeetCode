bd32c20d6d0ac584cc0042818b668c205983f409
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-01-30 10:52:42 +0530

    Time: 0 ms (100%), Space: 9.3 MB (87.47%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,18 +1,25 @@
 class MyStack {
 public:
     queue<int> q1;
-    queue<int> q2;
+    // queue<int> q2;
     MyStack() {}
     
     void push(int x) {
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
-        }
+        // while(!q1.empty()){
+        //     q2.push(q1.front());
+        //     q1.pop();
+        // }
+        // q1.push(x);
+        // while(!q2.empty()){
+        //     q1.push(q2.front());
+        //     q2.pop();
+        // }
         q1.push(x);
-        while(!q2.empty()){
-            q1.push(q2.front());
-            q2.pop();
+        int size = q1.size();
+        while(size > 1){
+            q1.push(q1.front());
+            q1.pop();
+            size--;
         }
     }
     
