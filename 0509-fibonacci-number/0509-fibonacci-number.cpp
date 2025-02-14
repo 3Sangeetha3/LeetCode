d36829469d2d7a45e3f80f59b7db7b7191d743f5
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-02-14 11:48:45 +0530

    Time: 0 ms (100%), Space: 7.8 MB (34.31%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+    //dynamic programming 
     int fib(int n) {
-        if( n<=1) return n;
-        int a =0, b = 1;
-        for(int i=2; i<=n; i++){
-            int temp = b;
-            b = a+b;
-            a = temp;
+        if(n <= 1) return n;
+        int previous = 1, pp = 0, curr = 0;
+        for(int i=2;i<=n;i++){
+            curr = previous + pp;
+            pp = previous;
+            previous = curr;
         }
-        return b;
+        return curr;
     }
 };
