0297b00316944f7c453dccc896927fe79991ecb3
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-03-03 20:13:42 +0530

    Time: 7 ms (59.22%), Space: 127.6 MB (87.5%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -2,28 +2,22 @@ class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
         int n = nums.size();
-        vector<int> lesser;
-        vector<int> equal;
-        vector<int> greater;
-        vector<int> res;
+        vector<int> res(n);
+        int countLess = 0, countEqual = 0;
         for(int i=0;i<n;i++){
-            if(nums[i] < pivot){
-                lesser.push_back(nums[i]);
-            }else if(nums[i] == pivot){
-                equal.push_back(nums[i]);
-            }else {
-                greater.push_back(nums[i]);
-            }
+            if(nums[i] < pivot) countLess++;
+            if(nums[i] == pivot) countEqual++;
         }
 
-        for(int i=0;i<lesser.size();i++){
-            res.push_back(lesser[i]);
-        }
-        for(int i=0;i<equal.size();i++){
-            res.push_back(equal[i]);
-        }
-        for(int i=0;i<greater.size();i++){
-            res.push_back(greater[i]);
+        int i = 0, j = countLess, k = countLess + countEqual;
+        for(int ele=0;ele<n;ele++){
+            if(nums[ele] < pivot){
+                res[i++] = nums[ele];
+            }else if(nums[ele] > pivot){
+                res[k++] = nums[ele];
+            }else {
+                res[j++] = nums[ele];
+            }
         }
         return res;
     }
