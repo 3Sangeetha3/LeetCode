f79b184b3fbc682e65e4707bb7765ea089758588
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-03-12 11:32:42 +0530

    Time: 0 ms (100%), Space: 7.7 MB (98.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int climbStairs(int n) {
         if(n<=3) return n;
-        int curr=0, prev = 3, pp = 2;
+        int curr = n, prev = 3, pp=2;
         for(int i=3;i<n;i++){
             curr = prev + pp;
             pp = prev;
