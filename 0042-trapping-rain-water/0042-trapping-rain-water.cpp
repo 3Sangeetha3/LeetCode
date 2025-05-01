09db2ae8ecca0a9e47d3be2ed78a9ceb92730b53
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-05-01 09:24:20 +0530

    Time: 0 ms (100%), Space: 27.4 MB (19.46%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -9,17 +9,16 @@ public:
         for(int i=1;i<n;i++){
             leftMax[i] = max(leftMax[i-1], height[i]);
         }
-
+        
         rightMax[n-1] = height[n-1];
         for(int i=n-2;i>=0;i--){
             rightMax[i] = max(rightMax[i+1], height[i]);
-        }
+        } 
 
         int water = 0;
         for(int i=0;i<n;i++){
-            water += (min(leftMax[i], rightMax[i]) - height[i]);
+            water += min(leftMax[i], rightMax[i]) - height[i];
         }
-
         return water;
     }
 };
