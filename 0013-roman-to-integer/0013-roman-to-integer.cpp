0fb5fa5049865c57eac6544b06d0b4ef5b638aa2
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2025-06-14 13:51:22 +0530

    Time: 9 ms (33.79%), Space: 13.2 MB (47.53%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -3,13 +3,7 @@ public:
     int romanToInt(string s) {
         int res = 0;
         unordered_map<char, int> roman = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
+            {'I', 1},{'V', 5},{'X', 10},{'L', 50},{'C', 100},{'D', 500},{'M', 1000}
         };
 
         for(int i=0;i<s.size()-1;i++){
