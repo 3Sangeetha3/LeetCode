177ab28f7f0273a946eca2c6251fabb57ca16209
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-04-20 13:00:07 +0530

    0001-two-sum

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -3,10 +3,9 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
         unordered_map<int, int> mp;
-        int complement = 0;
 
         for(int i=0;i<n;i++){
-            complement = target - nums[i];
+            int complement = target - nums[i];
 
             if(mp.find(complement) != mp.end()){ //exisits in map
                 return { mp[complement], i};
