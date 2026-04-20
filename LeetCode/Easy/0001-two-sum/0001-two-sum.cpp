3f24ade2206e294758017a5b9acdb4b53a0dec8e
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-04-20 12:35:10 +0530

    0001-two-sum

--- LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
+++ LeetCode/Easy/0001-two-sum/0001-two-sum.cpp
@@ -2,15 +2,14 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
-        vector<int> res;
+
         for(int i=0;i<n-1;i++){
-            for(int j=i+1; j<n; j++){
-                if(nums[i]+nums[j] == target){
-                    res.push_back(i);
-                    res.push_back(j);
+            for(int j=i+1;j<n;j++){
+                if(nums[i] + nums[j] == target){
+                    return {i, j};
                 }
             }
         }
-    return res;
+        return {};
     }
 };
