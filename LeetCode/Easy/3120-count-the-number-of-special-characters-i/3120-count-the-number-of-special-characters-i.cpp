3e73edcc39ea1730994e8930caea155541273b56
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-05-26 10:44:47 +0530

    3120-count-the-number-of-special-characters-i

--- LeetCode/Easy/3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
+++ LeetCode/Easy/3120-count-the-number-of-special-characters-i/3120-count-the-number-of-special-characters-i.cpp
@@ -3,11 +3,11 @@ public:
     int numberOfSpecialChars(string word) {
         int n = word.size();
         int count = 0;
-        unordered_set<int> ascii;
-        for(int i=0;i<n;i++){
-            ascii.insert(word[i]);
-            cout<<word[i];
-        }
+        unordered_set<int> ascii(word.begin(), word.end());
+        // for(int i=0;i<n;i++){
+        //     ascii.insert(word[i]);
+        //     cout<<word[i];
+        // }
 
         for(char ch='a'; ch<='z'; ch++){
             if(ascii.count(ch) && ascii.count(ch - 32)) count++;
