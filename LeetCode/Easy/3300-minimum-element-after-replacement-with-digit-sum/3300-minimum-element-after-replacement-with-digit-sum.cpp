8f9a7875c602068eceac657f22fd6f76b45ac28d
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-05-29 10:16:06 +0530

    3300-minimum-element-after-replacement-with-digit-sum

--- LeetCode/Easy/3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
+++ LeetCode/Easy/3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     int minElement(vector<int>& nums) {
-        int ans = INT_MAX;
-        for(int num: nums){
-            string s = to_string(num);
-            int sum = 0;
-            for(char ch : s){
-                sum += ch - '0';
-            }
-            ans = min(ans, sum);
+        int res = 36;
+        for(int n: nums){
+            res = min(res, n - 9 * ((n/10) + (n/100) + (n/1000) + (n/10000)));
         }
-    return ans;
+        return res;
     }
 };
