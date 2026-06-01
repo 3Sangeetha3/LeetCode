c6f9285cea484ab5a900c3b5a8184d2b74205550
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-01 10:55:57 +0530

    2144-minimum-cost-of-buying-candies-with-discount

--- LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
+++ LeetCode/Easy/2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.cpp
@@ -3,13 +3,13 @@ public:
     int minimumCost(vector<int>& cost) {
         int n = cost.size();
         int mini_cost = 0;
-        sort(cost.begin(), cost.end(), greater<int>());
-        for(int val: cost){
-            mini_cost += val;
+        sort(cost.begin(), cost.end());
+
+        for(int i=n-1;i>=0;i-=3){
+            if(i-1>=0) mini_cost += cost[i] + cost[i-1];
+            else mini_cost += cost[i];
         }
-        for(int i=0; i<n-2; i+=3){
-            mini_cost -= cost[i+2];
-        }
+
         return mini_cost;
     }
 };
