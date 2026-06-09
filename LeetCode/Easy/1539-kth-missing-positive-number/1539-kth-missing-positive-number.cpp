a75f67e9cfa1a290c880b94ac7ea2c8ee47b3cee
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-09 12:35:42 +0530

    1539-kth-missing-positive-number

--- LeetCode/Easy/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ LeetCode/Easy/1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,11 +1,25 @@
 class Solution {
 public:
+    int checkExists(vector<int>& arr, int ele){
+        int low = 0, high = arr.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(arr[mid] == ele){
+                return 1;
+            }else if(arr[mid] > ele){
+                high = mid - 1;
+            }else{
+                low = mid + 1;
+            }
+        }
+        return 0;
+    }
     int findKthPositive(vector<int>& arr, int k) {
         int n = arr.size();
         int end = arr[n-1] + k;
         int cnt = 0;
         for(int i=1;i<=end;i++){
-            if(count(arr.begin(), arr.end(), i) == 0){
+            if(!checkExists(arr, i)){
                 cnt++;
             }
             if(cnt == k){
