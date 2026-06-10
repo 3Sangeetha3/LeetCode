53c17f0cd0c98d5d7d07ff00e9fe8898a821cb11
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-10 16:28:56 +0530

    0014-longest-common-prefix

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        string first_str = strs[0];
-        for(int i=0;i<first_str.length();i++){
-            string prefix = first_str.substr(0, i+1);
-            for(int j=1;j<strs.size();j++){
-                if(strs[j].find(prefix) != 0){
-                    return first_str.substr(0, i);
-                }
+        sort(strs.begin(), strs.end());
+        string firstStr = strs[0], lastStr = strs[strs.size()-1];
+        string res = "";
+        for(int i=0;i<firstStr.size();i++){
+            if(firstStr[i] != lastStr[i]){
+                res = firstStr.substr(0,i);
+                break;
             }
+            res = firstStr.substr(0, i+1);
         }
-        return first_str;
+        return res;
     }
 };
