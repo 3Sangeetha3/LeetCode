c154b7ce002051eb573a56f167a708662b64146c
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-15 12:32:18 +0530

    0205-isomorphic-strings

--- LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ LeetCode/Easy/0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char, int> charS;
-        unordered_map<char, int> charT;
-
+        unordered_map<char, int> mp1, mp2;
         for(int i=0;i<s.size();i++){
-            if(charS.find(s[i]) == charS.end()){
-                charS[s[i]] = i;
+            if(mp1.find(s[i]) == mp1.end()){
+                mp1[s[i]] = i;
             }
-            if(charT.find(t[i]) == charT.end()){
-                charT[t[i]] = i;
+            if(mp2.find(t[i]) == mp2.end()){
+                mp2[t[i]] = i;
             }
-            if(charS[s[i]] != charT[t[i]]){
+            if(mp1[s[i]] != mp2[t[i]]){
                 return false;
             }
         }
