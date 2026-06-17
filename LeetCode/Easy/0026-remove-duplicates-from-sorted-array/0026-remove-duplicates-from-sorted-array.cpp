14c685ad763b8e45d420fbd171de490951952493
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-17 12:13:00 +0530

    0026-remove-duplicates-from-sorted-array

---
+++ LeetCode/Easy/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i=0, j=i+1;
+        if(n==1) return 1;
+        while(j<n){
+            if(nums[j] == nums[i]){
+                j++;
+            }else{
+                swap(nums[i+1], nums[j]);
+                i++;
+                j++;
+            }
+        }
+        return i+1;
+    }
+};
