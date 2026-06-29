e5bdb76447cea0481b3095f76c1c1559c638c83e
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-06-29 10:58:38 +0530

    0344-reverse-string

--- LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
+++ LeetCode/Easy/0344-reverse-string/0344-reverse-string.cpp
@@ -2,11 +2,10 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         int n = s.size();
-        int low = 0, high = n-1;
-        while(low < high){
-            swap(s[low], s[high]);
-            low++;
-            high--;
+        int l=0, r=n-1;
+        while(l<r){
+            swap(s[l], s[r]);
+            l++;r--;
         }
     }
 };
