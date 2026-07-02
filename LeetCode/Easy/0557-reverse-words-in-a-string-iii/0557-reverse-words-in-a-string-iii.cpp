3a48f060ae1ec82123a541ec13f12f5cd81e9dcb
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-02 17:37:24 +0530

    0557-reverse-words-in-a-string-iii

--- LeetCode/Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
+++ LeetCode/Easy/0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -3,18 +3,13 @@ public:
     string reverseWords(string s) {
         int n = s.size();
         int i=0;
-        string res = "";
-
         for(int j=0;j<=n;j++){
             if(j==n || s[j] == ' '){
-                string str = s.substr(i, j-i);
-                reverse(str.begin(), str.end());
-                res += str;
-                if(j!=n) res += " ";
+                reverse(s.begin() + i, s.begin() + j);
                 i=j+1;
             }
         }
 
-        return res;
+        return s;
     }
 };
