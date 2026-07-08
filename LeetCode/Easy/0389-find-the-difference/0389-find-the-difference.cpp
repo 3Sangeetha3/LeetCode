1c5546bd17ad3a579690f9b40c9a271657d06385
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-08 11:42:38 +0530

    0389-find-the-difference

--- LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.cpp
+++ LeetCode/Easy/0389-find-the-difference/0389-find-the-difference.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     char findTheDifference(string s, string t) {
-        vector<int> freq(26, 0);
-        char ans;
-        for(char ch: t){
-            freq[ch - 'a']++;
-        }
-
+        char ans = 0;
         for(char ch: s){
-            freq[ch-'a']--;
+            ans ^= ch;
         }
-
         for(char ch: t){
-            if(freq[ch - 'a'] != 0){
-                ans = ch;
-            }
+            ans ^= ch;
         }
         return ans;
     }
