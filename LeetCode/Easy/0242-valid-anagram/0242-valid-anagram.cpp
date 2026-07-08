2b521f9d7c4ccaa955ac778252f22fdda6e3e4b4
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-08 11:52:39 +0530

    0242-valid-anagram

--- LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
+++ LeetCode/Easy/0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,17 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        int n = s.size(), m = t.size();
         vector<int> freq(26, 0);
-        if(n!=m) return false;
-        for(int i=0;i<n;i++)freq[s[i]-'a']++;
-        for(int i=0;i<m;i++)freq[t[i]-'a']--;
-        for(int i=0;i<26;i++)if(freq[i]!=0) return false;
+        for(char ch: s){
+            freq[ch - 'a']++;
+        }
+        for(char ch: t){
+            freq[ch-'a']--;
+        }
+
+        for(int i=0;i<26;i++){
+            if(freq[i] != 0) return false;
+        }
         return true;
     }
 };
