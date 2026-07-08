44748d0bc6dc101daa081dc6076082d01063af1a
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-08 11:05:48 +0530

    0520-detect-capital

--- LeetCode/Easy/0520-detect-capital/0520-detect-capital.cpp
+++ LeetCode/Easy/0520-detect-capital/0520-detect-capital.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
-    bool check(string word, char start, char end){
-        for(char ch: word){
-            if(ch < start || ch > end){
-                return false;
-            }
-        }
-        return true;
-    }
-
     bool detectCapitalUse(string word) {
         int n = word.size();
-        if(check(word, 'A', 'Z') || check(word, 'a', 'z') || check(word.substr(1, n), 'a', 'z')){
-            return true;
+        int c_count = 0;
+        for(char ch: word){
+            if(ch >= 'A' && ch <= 'Z')
+            c_count++;
         }
+
+        if(c_count == n || c_count == 0 || (c_count == 1  && isupper(word[0]))) return true;
         return false;
     }
 };
