bcac0647e134bec5740e835275625f7e49f03026
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-14 17:31:30 +0530

    2000-reverse-prefix-of-word

--- LeetCode/Easy/2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
+++ LeetCode/Easy/2000-reverse-prefix-of-word/2000-reverse-prefix-of-word.cpp
@@ -2,14 +2,7 @@ class Solution {
 public:
     string reversePrefix(string word, char ch) {
         int n = word.size();
-        int ch_i = 0;
-        for(int i=0;i<n;i++){
-            if(word[i] == ch){
-                ch_i = i;
-                break;
-            }
-        }
-
+        int ch_i = word.find(ch);
         reverse(word.begin(), word.begin() + ch_i + 1);
 
         return word;
