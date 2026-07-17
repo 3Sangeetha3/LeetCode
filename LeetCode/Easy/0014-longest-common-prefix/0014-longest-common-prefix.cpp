531733568e4525b7ff7bb3d84c6278fec6cdfc40
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-17 12:40:25 +0530

    0014-longest-common-prefix

--- LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ LeetCode/Easy/0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -3,13 +3,15 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         int n = strs.size();
         sort(strs.begin(), strs.end());
-        string first_str = strs[0], last_str = strs[n-1];
-        int len = min(first_str.size(), last_str.size());
+        string first = strs[0],last = strs[n-1];
+        int len = min(first.size(), last.size());
+
         for(int i=0;i<len;i++){
-            if(first_str[i] != last_str[i]){
-                return first_str.substr(0, i);
+            if(first[i] != last[i]){
+                return first.substr(0,i);
             }
         }
-        return first_str.substr(0, len);
+        
+        return first.substr(0, len);
     }
 };
