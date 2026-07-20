b3dbfb7de9097b66f1b376762950981c651aaea0
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-20 18:41:10 +0530

    1260-shift-2d-grid

--- LeetCode/Easy/1260-shift-2d-grid/1260-shift-2d-grid.cpp
+++ LeetCode/Easy/1260-shift-2d-grid/1260-shift-2d-grid.cpp
@@ -3,20 +3,25 @@ public:
     vector<vector<int>> shiftGrid(vector<vector<int>>& grid, int k) {
         int rows = grid.size(), cols = grid[0].size();
         int n = rows * cols;
-        k%=n;
-        if(k==0) return grid;
-        
-        auto reverse = [&](int i, int j) {
-            while(i<j){
-                swap(grid[i/cols][i%cols], grid[j/cols][j%cols]);
-                i++;
-                j--;
-            }
-        };
+        vector<int> arr(n);
 
-        reverse(0, n-k-1);
-        reverse(n-k, n-1);
-        reverse(0, n-1);
+        int idx = 0;
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                arr[idx++] = grid[i][j];
+            }
+        }
+
+        k%=n;
+        reverse(arr.begin(), arr.begin()+(n-k));
+        reverse(arr.begin()+(n-k), arr.end());
+        reverse(arr.begin(), arr.end());
+
+        for(int i=0;i<n;i++){
+            int row = i/cols;
+            int col = i%cols;
+            grid[row][col] = arr[i];
+        }
 
         return grid;
     }
