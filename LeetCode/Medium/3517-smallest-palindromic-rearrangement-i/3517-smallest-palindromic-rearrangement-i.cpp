10fda32fdc7fa26dad8569e5e7f9d7ac178f6527
sangeetha jadamal <117072987+3Sangeetha3@users.noreply.github.com>
2026-07-28 16:52:58 +0530

    3517-smallest-palindromic-rearrangement-i

---
+++ LeetCode/Medium/3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string smallestPalindrome(string s) {
+        int n = s.size();
+        int mid = n / 2;
+
+        sort(s.begin(), s.begin()+mid);
+
+        int i=0, j=n-1;
+        while(i<j){
+            s[j] = s[i];
+            i++;j--;
+        }
+        return s;
+    }
+};
